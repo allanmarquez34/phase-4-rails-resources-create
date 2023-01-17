73b3cac555f6612c2b7d742f4bfb1ad24fce047c
Allan Marquez <allan.marquez.2003@icloud.com>
2023-01-17 12:23:47 -0700

    commit tree 85349fca4d41f8dc696aba01a2949a6d549b6b25

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -16,4 +16,10 @@ class BirdsController < ApplicationController
     end
   end
 
+  #POST /birds 
+  def create 
+    bird = Bird.create(name: ???, species: ???)
+    render json: bird, status: :created
+  end 
+
 end
